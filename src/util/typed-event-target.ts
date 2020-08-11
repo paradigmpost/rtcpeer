a1526caf96acce1fc9d156bc81711026fc9a931f
Sam Moore <sam@toldmedia.com>
2020-08-11 23:27:28 +0000

    fix(util/typed-event-target): fix type system:
    - revert back to 3 overload methods so mapped events can be inferred by
      the event type specified
    - explicitly ignore typescript error on type cast of EventHandler, which
      should only break type guarentees when non-mapped event types are used

--- src/util/typed-event-target.ts
+++ src/util/typed-event-target.ts
@@ -1,24 +1,22 @@
-interface AnyEventMap {
+export interface AnyEventMap {
   [key: string]: Event;
 }
 
-type Keys<T> = Extract<keyof T, string>;
+export type Keys<T> = Extract<keyof T, string>;
 
 export type SomeEventMap<T extends AnyEventMap> = {
-  [key in Keys<T>]: T[Keys<T>];
+  [key in Keys<T>]: T[key];
 };
 
-// WARNING: this does not enforce that values of the EventMap are Event's
-// to workaround this temporarily, E is defined as 'EventMap[K] & Event'
 export class TypedEventTarget<T extends AnyEventMap, EventMap = SomeEventMap<T>>
   extends EventTarget
 {
   addEventListener<
-    K extends Extract<keyof EventMap, string>,
+    K extends Keys<EventMap>,
     E extends EventMap[K]
   >(
     type: K,
-    listener: (ev: E | Event) => void,
+    listener: (ev: E) => void,
     options?: boolean | AddEventListenerOptions
   ): void
 
@@ -26,7 +24,52 @@ export class TypedEventTarget<T extends AnyEventMap, EventMap = SomeEventMap<T>>
     type: string,
     listener: EventListenerOrEventListenerObject | null,
     options?: boolean | AddEventListenerOptions
+  ): void
+
+  addEventListener<
+    K extends Keys<EventMap>,
+    E extends EventMap[K]
+  >(
+    type: K | string,
+    listener: (ev: E) => void | EventListenerOrEventListenerObject | null,
+    options?: boolean | AddEventListenerOptions
   ): void {
-    super.addEventListener(type, listener as EventListenerOrEventListenerObject, options);
+    super.addEventListener(
+      type,
+      // @ts-expect-error 2352
+      listener as EventListenerOrEventListenerObject,
+      options
+    );
   }
+
+  removeEventListener<
+    K extends Keys<EventMap>,
+    E extends EventMap[K]
+  >(
+    type: K,
+    listener: (ev: E) => void,
+    options?: boolean | EventListenerOptions
+  ): void
+
+  removeEventListener(
+    type: string,
+    listener: EventListenerOrEventListenerObject | null,
+    options?: boolean | EventListenerOptions
+  ): void
+
+  removeEventListener<
+    K extends Keys<EventMap>,
+    E extends EventMap[K]
+  >(
+    type: K | string,
+    listener: (ev: E) => void | EventListenerOrEventListenerObject | null,
+    options?: boolean | EventListenerOptions
+  ): void {
+    super.removeEventListener(
+      type,
+      // @ts-expect-error 2352
+      listener as EventListenerOrEventListenerObject,
+      options
+    );
+  };
 }
