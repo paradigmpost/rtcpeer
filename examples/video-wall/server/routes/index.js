ee4b5fb3c2027be755bf675133a0d464493a5159
Sam Moore <sam@toldmedia.com>
2021-11-12 23:35:38 +0000

    wip!

--- examples/video-wall/server/routes/index.js
+++ examples/video-wall/server/routes/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import uuid from 'uuid';
+
+import db from '../db';
 
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(_, res, _) {
-  res.render('index', { title: 'Express' });
+/* POST to get a uuid for peering. */
+router.post('/', function(_, res, _) {
+  const id = uuid.v4();
+  db[id] = {};
+
+  res.statusCode(201).json({ id }).end();
 });
 
 export default router;
